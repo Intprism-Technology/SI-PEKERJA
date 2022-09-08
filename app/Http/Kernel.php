b6ebb63705ad92a3d2eabc99426f3d420167fefb
Muhammad Habib Ulil A <habibulilalbaab@intprism.com>
2022-09-09 06:36:44 +0700

    Update Kernel.php
    update rate limit

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -40,7 +40,8 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-            'throttle:api',
+            // 'throttle:api',
+            'throttle:120,1',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
     ];
